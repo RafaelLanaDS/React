30769b5df081327aa3ec738476341fd33c5f3a8e
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-05-12 14:28:31 -0300

    commit

--- Projeto/costs/src/App.jsx
+++ Projeto/costs/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <p>const</p>
+      <p>Const</p>
     </div>
   )
 }
