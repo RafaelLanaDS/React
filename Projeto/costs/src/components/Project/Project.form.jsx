9c62ec3623410f47a743698e2625261896e25c1a
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-05-17 12:16:00 -0300

    commit

--- Projeto/costs/src/components/Project/Project.form.jsx
+++ Projeto/costs/src/components/Project/Project.form.jsx
@@ -1,27 +1,32 @@
+import Imput from '../form/Imput'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubimitButton'
+import styles from './ProjectForm.module.css'
 
 
-function ProjectForm(){
+function ProjectForm({btnText}){
     return(
-        <form >
-            <div>
-                <label htmlFor="name">Nome do projeto:</label>
-                <input type="text" placeholder="Nome do projeto" id="name" name="name" />
-            </div>
-            <div>
-                <label htmlFor="budget">Orçamento:</label>
-                <input type="number" placeholder="Orçamento" id="budget" name="budget" />
-            </div>
-            <div>
-                <label htmlFor="category_id">Categoria:</label>
-                <select name="category_id" id="category_id">
-                    <option value="" disabled selected>Selecione uma categoria</option>
-                    <option value="1">Categoria 1</option>
-                    <option value="2">Categoria 2</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar projeto" />
-            </div>
+        <form className={styles.form}>
+            <Imput 
+            type='text' 
+            text='Nome do projeto' 
+            name='name' 
+            placeholder='Insira o nome do projeto'
+            />
+
+            <Imput 
+            type='number' 
+            text='Orçamento do projeto' 
+            name='budget' 
+            placeholder='Insira o orçamento do projeto'
+            />
+
+            <Select
+            name='category_id'  
+            text='Selecione a categorioa'/>
+
+            <SubmitButton 
+            text={btnText}/>
         </form>
     )
 }
