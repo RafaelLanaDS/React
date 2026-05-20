0b006b8f4f0030f73ec333012f61675f8aa56a6f
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-05-19 21:56:54 -0300

    commit

--- Projeto/costs/src/components/pages/Projects.jsx
+++ Projeto/costs/src/components/pages/Projects.jsx
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom";
 import Message from "../Layout/Message";
+import styles from "./projects.module.css"
+import Container from "../Layout/Container"
+import LinkButton from '../Layout/LinkButton'
 
 function Projects() {
 
@@ -10,9 +13,15 @@ function Projects() {
     }
 
     return (
-        <div>
-            <h1>Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Projetos</h1>
+                <LinkButton to="/NewProject" text="Criar Projeto" /> 
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>projetoss</p>
+            </Container>
         </div>
     );
 }
