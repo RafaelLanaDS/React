557b5f9d72236696bbb4e73cf69aa5bae81af19e
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-05-19 21:31:20 -0300

    commit

--- Projeto/costs/src/components/pages/Projects.jsx
+++ Projeto/costs/src/components/pages/Projects.jsx
@@ -1,9 +1,18 @@
-
+import { useLocation } from "react-router-dom";
+import Message from "../Layout/Message";
 
 function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
     return (
         <div>
-            <h1>Projects</h1>
+            <h1>Projetos</h1>
+            {message && <Message type="success" msg={message} />}
         </div>
     );
 }
