14127b96b9fec467410b84397f9eef1bfd6afdf2
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-05-25 20:23:54 -0300

    commit

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+  return (
+    <>
+      <h1>Fundamentos do react</h1> {/* comentario em jsx */}
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,10 @@
+
+const FirstComponent = () => { // Função
+    return(
+        <div>
+            <h1>meu primeiro componente</h1>
+        </div>
+    );
+};
+
+export default FirstComponent
