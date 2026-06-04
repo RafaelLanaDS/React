6aa10528174012fc31820759df3310f84ecbb18a
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-06-04 20:26:24 -0300

    commit

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
   return(
     <div className='app'>
+      
+      {/* CSS global */}
       <h1>React com css</h1>
+
+      {/* CSS componente */}
+      <MyComponent />
+      <p>este paragrafo do componente</p>
     </div>
   )
 }
-
 export default App
---
+++ 4_CSS_REACT/css/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Css de components</h1>
+      <p>este é o paragrafo do componente</p>
+      <p className="my-comp-paragraph">este tb e do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
