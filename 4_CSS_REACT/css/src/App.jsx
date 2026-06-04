3096c7ab7dba733e63648c0b7202f414b2a4d577
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-06-04 20:38:52 -0300

    commit

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -5,13 +5,16 @@ import MyComponent from './components/MyComponent'
 function App() {
   return(
     <div className='app'>
-      
+
       {/* CSS global */}
       <h1>React com css</h1>
 
       {/* CSS componente */}
       <MyComponent />
       <p>este paragrafo do componente</p>
+
+      {/* inlise css */}
+      <p style={{color: 'blue', padding: "25px", borderTop:"2px solid black"}}>este elemento foi estilizado de forma inline</p>
     </div>
   )
 }
