efda4333900019989045987d84423b69aa463df4
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-06-06 12:06:15 -0300

    commit

--- 5_FORM_EM_REACT/form/src/components/Myforms.jsx
+++ 5_FORM_EM_REACT/form/src/components/Myforms.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Myforms.module.css'
 
 const Myforms = () => {
+    // 3 - gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    };
+
+    // console.log(name)
+    // console.log(email)
+
+    const handleSubmit = (Event) => {
+        Event.preventDefault();
+        console.log("enviando o formulario")
+        console.log(name, email)
+    };
+
   return (
     <div>
-      {/* criação de formes */}
-      <form>
+      {/* 5 - envio de forme */}
+      {/* 1 - criação de forme */}
+      <form onSubmit={handleSubmit}>
         <div>
             <label htmlFor="name">Nome:</label>
-            <input type="text" name='name' placeholder='Digite seu nome' />
+            <input 
+                type="text" 
+                name='name' 
+                placeholder='Digite seu nome' 
+                onChange={handleName}
+            />
         </div>
 
-         {/* label envolvendo input */}
+         {/* 2- label envolvendo input */}
          <label>
             <span>E-mail</span>
-            <input type="email" name="email" id="email" placeholder='digite seu email'/>
+            {/* 4 - simplificação  de manipulação de state*/}
+            <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                placeholder='digite seu email'
+                onChange={(e) => setEmail(e.target.value)}
+            />
          </label>
         <input type="submit" value="Enviar" />
       </form>
