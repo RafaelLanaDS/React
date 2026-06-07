ccab269ee09f61b1e808d8e2a0729f08c2bca78e
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-06-07 14:45:09 -0300

    commit

--- 5_FORM_EM_REACT/form/src/App.jsx
+++ 5_FORM_EM_REACT/form/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Formularios em react</h1>
-      <Myforms />
+      <Myforms user={{name: 'Rafael', email:'rafael@gmail.com', bio: "exemplo de bio"}}/>
     </div>
     
   )
--- 5_FORM_EM_REACT/form/src/components/Myforms.jsx
+++ 5_FORM_EM_REACT/form/src/components/Myforms.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 import styles from './Myforms.module.css'
 
-const Myforms = () => {
+const Myforms = ({user}) => {
+    // 6 - controlled inputs
+
     // 3 - gerenciamento de dados
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+    const [name, setName] = useState(user ? user.name : '')
+    const [email, setEmail] = useState(user ? user.email : '')
+
+    const [bio, setBio] = useState(user ? user.bio : '')
 
     const handleName = (e) => {
         setName(e.target.value)
@@ -19,42 +23,64 @@ const Myforms = () => {
         // Event.preventDefault() → cancela o recarregamento da página
         // sem ele a página recarregaria e perderia os dados!
         console.log("enviando o formulario")
-        console.log(name, email)
+        console.log(name, email, bio)
+
+        //validação em cima 
+        //envio em baixo
+
+        // 7 - limpar form
+        setName('')
+        setEmail('')
+        setBio('')
     };
 
   return (
     <div>
         {/* 5 - envio de forme */}
         {/* 1 - criação de forme */}
-      <form onSubmit={handleSubmit}>
-        {/* onSubmit → escuta o envio do form e chama handleSubmit */}
-        <div>
-            <label htmlFor="name">Nome:</label>
-            <input 
-                type="text" 
-                name='name' 
-                placeholder='Digite seu nome' 
-                onChange={handleName}
-                // onChange → chama handleName a cada letra digitada
-                // forma separada → usada quando a lógica é mais complexa
-            />
-        </div>
+        <form onSubmit={handleSubmit}>
+            {/* onSubmit → escuta o envio do form e chama handleSubmit */}
+            <div>
+                <label htmlFor="name">Nome:</label>
+                <input 
+                    type="text" 
+                    name='name' 
+                    placeholder='Digite seu nome' 
+                    onChange={handleName}
+                    // onChange → chama handleName a cada letra digitada
+                    // forma separada → usada quando a lógica é mais complexa
+                    value={name}
+                />
+            </div>
+
             {/* 2- label envolvendo input */}
-         <label>
-            <span>E-mail</span>
-            {/* 4 - simplificação  de manipulação de state*/}
-            <input 
-                type="email" 
-                name="email" 
-                id="email" 
-                placeholder='digite seu email'
-                onChange={(e) => setEmail(e.target.value)}
-                // onChange → função inline direto no input
-                // forma simplificada → usada quando só precisa atualizar o state
-            />
-         </label>
-        <input type="submit" value="Enviar" />
-      </form>
+            <label>
+                <span>E-mail</span>
+                {/* 4 - simplificação  de manipulação de state*/}
+                <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    placeholder='digite seu email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    // onChange → função inline direto no input
+                    // forma simplificada → usada quando só precisa atualizar o state
+                    value={email}
+                />
+            </label>
+            {/* 8 - text area */}
+            <label htmlFor="">
+                <span>Bio</span>
+                <textarea  
+                    name="bio"   
+                    id="bio"  
+                    placeholder='descrição do usuario'   
+                    onChange={(e) => setBio(e.target.value)}  
+                    value={bio}>
+                </textarea>
+            </label>
+            <input type="submit" value="Enviar" />
+        </form>
     </div>
   )
 }
