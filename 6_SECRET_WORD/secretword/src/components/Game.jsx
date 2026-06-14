e1f22b8186617c2fa7bce1c6f8a8b46ed91f5f5c
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-06-14 15:38:55 -0300

    commit

---
+++ 6_SECRET_WORD/secretword/src/components/Game.jsx
@@ -0,0 +1,12 @@
+import "./Game.css"
+
+const Game = ({verifyLatter}) => {
+  return (
+    <div>
+      <h1>game</h1>
+      <button onClick={verifyLatter}>Finalizar jogo</button>
+    </div>
+  )
+}
+
+export default Game
---
+++ 6_SECRET_WORD/secretword/src/components/GameOver.jsx
@@ -0,0 +1,13 @@
+
+import './GameOver.css'
+
+const GameOver = ({retry}) => {
+  return (
+    <div>
+      <h1>Game over</h1>
+      <button onClick={retry}>Resetar jogo</button>
+    </div>
+  )
+}
+
+export default GameOver
