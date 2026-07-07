29d2e22f29af3ee187791e7808221bb79ab7e9e7
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-07-07 17:16:56 -0300

    commit

---
+++ Desafo_07/vite-project/src/hooks/useFetch.jsx
@@ -0,0 +1,89 @@
+import {useState, useEffect} from 'react'
+
+// 4 custom, hook
+
+export const useFetch = (url) => {
+    const [data, setData] = useState(null)
+
+    //5 - refatorando post
+    const [config, setConfig] = useState(null)
+    const [method, setMethod] = useState(null)
+    const [callFetch, setCallFetch] = useState(false)
+
+    // 6 - loading
+    const [loading, setLoading] = useState(false)
+
+    // 7 - tratando erros
+    const [error, setError] = useState(null)
+
+    // 8 - desafio 6
+    const [itemId, setItemId] = useState(null)
+
+    const httpConfig = (data, method) => {
+        if(method === 'POST') {
+            setConfig({
+                method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+
+            setMethod(method)
+        }else if(method === 'DELETE') {
+            setConfig({
+                method,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            setMethod(method)
+            setItemId(data) //data = id do item a ser deletado
+        }
+    }
+    useEffect(() => {
+
+        const fetchData = async () => {
+            // 6 - loading
+            setLoading(true)
+
+            try{
+                const res = await fetch(url)
+
+            const json = await res.json()
+
+            setData(json)
+            } catch(error) {
+                console.log(error.message)
+                setError('Houve algum erro ao carregar os dados.')
+            }
+
+            // 6 - loading
+            setLoading(false)
+        }
+
+        fetchData()
+    }, [url, callFetch]) //callFetch = quando mudar, executa o useEffect
+
+    // 5 - refatorando post
+    useEffect(() => {
+        const httpRequest = async () => {
+
+            if(method === 'POST') {
+                let fetchOptions = [url, config]
+
+                const res = await fetch(...fetchOptions)
+                const json = await res.json()
+                setCallFetch(json)
+            }else if(method === 'DELETE') {
+                const deleteUrl = `${url}/${itemId}` //itemId = id do item a ser deletado
+                const res = await fetch(deleteUrl, config)
+                const json = await res.json()
+                setCallFetch(json)
+            }
+        }
+        httpRequest()
+    }, [config, method, url, itemId]) //config = quando mudar, executa o useEffect
+
+    return {data, httpConfig, loading, error}
+}
