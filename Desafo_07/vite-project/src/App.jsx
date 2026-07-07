b6ebfac6b7f6b98866d27f83b9156c4f10ebe239
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-07-06 22:22:15 -0300

    commit

--- Desafo_07/vite-project/src/App.jsx
+++ Desafo_07/vite-project/src/App.jsx
@@ -7,12 +7,17 @@ import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 
+// components
+import NavBar from './components/NavBar'
+
 function App() {
 
   return (
     <div className="App">
+
       <h1>React Router</h1>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sobre" element={<Sobre />} />
