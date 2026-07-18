98883600c71bf34ef961eb7f1fa1afef58cf7496
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-07-18 12:54:10 -0300

    commit

--- Context/Context/src/Pages/Home.jsx
+++ Context/Context/src/Pages/Home.jsx
@@ -1,10 +1,16 @@
 import './Home.css'
+import { useContext } from 'react'
+import { CounterContext } from '../context/CounterContext'
+
 
 const Home = () => {
+  const { counter, setCounter } = useContext(CounterContext);
+
   return (
     <div>
       <h1>Home</h1>
       <p>Esta é a página Home.</p>
+      <p>Contador: {counter}</p>
     </div>
   )
 }
