8aa277e94809627e2a024b8dcd794111e7653c78
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-08-23 12:50:35 -0300

    coomit

--- 11_HOOKS/hooks/src/components/HookUseState.jsx
+++ 11_HOOKS/hooks/src/components/HookUseState.jsx
@@ -13,6 +13,16 @@ const HookUseState = () => {
 
     }
 
+    // 2 - useState e input
+    const [age, setAge] = useState(18)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        //envio a uma api
+        console.log(age)
+    }
+
+
     return (
         <div>
             {/* 1 - useState */}
@@ -20,6 +30,15 @@ const HookUseState = () => {
             <p>Variavel: {userName}</p>
             <p>useState: {name}</p>
             <button onClick={changeName}>Mudar variaveis</button>
+            <hr />
+            
+            {/* 2 - useState e input */}
+            <h2>useState com input</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="number" value={age} onChange={(e) => setAge(parseInt(e.target.value))} />
+                <input type="submit" value="Enviar" />
+            </form>
+            <p>Voce tem {age} anos</p>
             <hr />
         </div>
     )
