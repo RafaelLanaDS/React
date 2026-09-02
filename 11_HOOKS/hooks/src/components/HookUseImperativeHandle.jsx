a0597b3de9bba9beaab74d9160197a00d201ddb0
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-09-02 19:14:36 -0300

    commit

--- 11_HOOKS/hooks/src/components/HookUseImperativeHandle.jsx
+++ 11_HOOKS/hooks/src/components/HookUseImperativeHandle.jsx
@@ -1,16 +1,17 @@
-import {useRef} from 'react'
-import SomeComponent from './SomeComponent'
+import { useRef } from "react";
+import SomeComponent from "./SomeComponent";
 
 const HookUseImperativeHandle = () => {
-    const inputRef = useRef()
+  const inputRef = useRef();
 
   return (
     <div>
-      <h2>UseImperativeHandle</h2>
+      <h2>useImperativeHandle</h2>
       <SomeComponent ref={inputRef} />
+      <button onClick={() => inputRef.current.validate()}>Validate</button>
       <hr />
     </div>
-  )
-}
+  );
+};
 
-export default HookUseImperativeHandle
+export default HookUseImperativeHandle;
