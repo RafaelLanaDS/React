350defdd2ebd0713dcb3c17e4e6908ffa6ff092e
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-09-02 19:12:40 -0300

    commit

--- 11_HOOKS/hooks/src/components/HookUseImperativeHandle.jsx
+++ 11_HOOKS/hooks/src/components/HookUseImperativeHandle.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import {useRef} from 'react'
+import SomeComponent from './SomeComponent'
 
 const HookUseImperativeHandle = () => {
+    const inputRef = useRef()
+
   return (
     <div>
       <h2>UseImperativeHandle</h2>
+      <SomeComponent ref={inputRef} />
       <hr />
     </div>
   )
---
+++ 11_HOOKS/hooks/src/components/SomeComponent.jsx
@@ -0,0 +1,12 @@
+import {forwardRef, useRef, useImperativeHandle} from 'react'
+
+const SomeComponent = forwardRef((props, ref) => {
+    const localInputRef = useRef()
+    return (
+        <div>
+         SomeComponent
+        </div>
+    )
+})
+
+export default SomeComponent
