6e3cebefd8763edd6b6387710cf04833597402a0
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-09-04 23:05:45 -0300

    commit

--- 11_HOOKS/hooks/src/Hook/UsePrevious.jsx
+++ 11_HOOKS/hooks/src/Hook/UsePrevious.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useDebugValue } from "react";
 
 export const usePrevious = (value) => {
-  const ref = useRef
+  const ref = useRef(); // Correção pequena: chamar como função useRef()
 
   useEffect(() => {
-    ref.current = value
-  })
+    ref.current = value; // Atualiza o valor armazenado APÓS a renderização
+  });
 
-  return ref.current
-}
+  useDebugValue(ref.current); // Exibe o valor anterior na extensão React DevTools
+
+  return ref.current; // Retorna o valor antigo (da renderização anterior)
+};
